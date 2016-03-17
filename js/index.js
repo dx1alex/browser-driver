f01844492b3b48e0df6fd121de0a1c15cc6fd5a8
dx1 <dx1@mail.ru>
2016-03-18 01:27:56 +0600

    ...

--- js/index.js
+++ js/index.js
@@ -7,5 +7,7 @@ __export(require('./lib/chrome'));
 __export(require('./lib/element'));
 var browser_sync_1 = require('./lib/browser-sync');
 exports.BrowserSync = browser_sync_1.BrowserSync;
+var chrome_sync_1 = require('./lib/chrome-sync');
+exports.ChromeSync = chrome_sync_1.ChromeSync;
 var element_sync_1 = require('./lib/element-sync');
 exports.ElementSync = element_sync_1.ElementSync;
