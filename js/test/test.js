17e02631dbf0a0db4ea383a2695671bb96eb8bd5
dx1 <dx1alex@gmail.com>
2016-11-19 15:54:55 +0700

    ...

--- js/test/test.js
+++ js/test/test.js
@@ -1,13 +1,14 @@
+#!/usr/bin/env node
 "use strict";
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     return new (P || (P = Promise))(function (resolve, reject) {
         function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator.throw(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
         function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
         step((generator = generator.apply(thisArg, _arguments)).next());
     });
 };
-const index_1 = require('../index');
+const index_1 = require("../index");
 const bro = new index_1.Chrome({
     dir: '/tmp/test2',
     init: { url: 'http://localhost:9515' },
--- test/test.ts
+++ test/test.ts
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 import { Chrome } from '../index'
 
 const bro = new Chrome({
